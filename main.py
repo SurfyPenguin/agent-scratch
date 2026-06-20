8944e4c874547cbe07b4b2e6d4c6e3aee1533f1c
SurfyPenguin <186186766+SurfyPenguin@users.noreply.github.com>
2026-06-20 15:47:25 +0530

    feat: add write tool

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from google.genai.types import GenerateContentConfig
 from models import gemini_models
 from config import GEMINI_API_KEY
 
-MODEL: gemini_models = "gemini-2.5-flash"
+MODEL: gemini_models = "gemini-3.1-flash-lite"
 EXIT_COMMANDS = ["/exit", "/quit"]
 
 client = genai.Client(api_key=GEMINI_API_KEY)
@@ -20,12 +20,23 @@ def read_file(file_path: str) -> str:
         return f"Error: File at `{file_path}` was not found."
     except IOError as e:
         return f"Error reading file `{file_path}`: {e}"
+    
+def write_file(file_path: str, content: str) -> str:
+    """
+    Use this tool when the user asks you to write, create, or edit a file.
+    """
+    try:
+        with open(file_path, "w") as file:
+            file.write(content)
+        return f"Successfully wrote to `{file_path}`."
+    except IOError as e:
+        return f"Error writing to file `{file_path}`: {e}"
 
 system_prompt = "You are a senior software developer, who specializes in design patterns and system architecture."
 
 config = GenerateContentConfig(
     system_instruction=system_prompt,
-    tools=[read_file]
+    tools=[read_file, write_file]
 )
 
 def main() -> None:
