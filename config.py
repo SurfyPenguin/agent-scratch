6cf00fcfdb8713cf498c6c4773b1845ecbae6d88
SurfyPenguin <186186766+SurfyPenguin@users.noreply.github.com>
2026-06-22 11:59:30 +0530

    feat: add dotenv support

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
 import os
+from dotenv import load_dotenv
 
 from core.gemini_models import gemini_models
 
-try:
-    GEMINI_API_KEY = os.environ["GEMINI_API_KEY"]
-except KeyError:
-    print("Error: gemini api key not found in environment")
+load_dotenv()
 
-# TODO: don't hardcode model in code
-GEMINI_MODEL: gemini_models = "gemini-3.1-flash-lite"
+GEMINI_API_KEY = os.environ["GEMINI_API_KEY"]
+GEMINI_MODEL: gemini_models = os.environ["GEMINI_MODEL"]
